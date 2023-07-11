fa759616244e524815584d94e1a635dea7cd8ebf
pipe6496651 <46767951+pipe6496651@users.noreply.github.com>
2023-07-11 11:18:36 -0500

    Update CUDA_trapezAreaSimple.cu

--- Cuda/CUDA_trapezAreaSimple.cu
+++ Cuda/CUDA_trapezAreaSimple.cu
@@ -3,11 +3,13 @@
 #include <cuda_runtime.h>
 #include <sys/time.h>
 
-__host__ __device__ double f(double x){
+// Definición de la función f(x)
+_host_ _device_ double f(double x){
   return x*x;
 }
 
-__device__ double atomicAddDouble(double* address, double val) {
+// Implementación de la función atomicAdd para números de punto flotante de doble precisión
+_device_ double atomicAddDouble(double* address, double val) {
   unsigned long long int* address_as_ull = (unsigned long long int*)address;
   unsigned long long int old = *address_as_ull, assumed;
   do {
@@ -18,7 +20,8 @@ __device__ double atomicAddDouble(double* address, double val) {
   return __longlong_as_double(old);
 }
 
-__global__ void integrate(double a, double b, int n, double h, double* result) {
+// Kernel para calcular la integral utilizando CUDA
+_global_ void integrate(double a, double b, int n, double h, double* result) {
   int i;
   double x, sum = 0.0;
   int idx = threadIdx.x + blockIdx.x * blockDim.x;
@@ -34,6 +37,7 @@ __global__ void integrate(double a, double b, int n, double h, double* result) {
   atomicAddDouble(result, sum);
 }
 
+// Función para obtener el tiempo actual en segundos con precisión de microsegundos
 double getCurrentTime() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
@@ -46,41 +50,53 @@ int main(){
   double* result;
   double* dev_result;
   
-  /*Ask the user for necessary input */
-  printf("\nEnter the no. of sub-intervals: ");
+  // Solicitar al usuario la entrada necesaria
+  printf("\nIngrese el número de subintervalos: ");
   scanf("%d", &n);
-  printf("\nEnter the initial limit: ");
+  printf("\nIngrese el límite inicial: ");
   scanf("%lf", &a);
-  printf("\nEnter the final limit: ");
+  printf("\nIngrese el límite final: ");
   scanf("%lf", &b);
   
+  // Calcular el tamaño del intervalo
   h = fabs(b - a) / n;
   
-  blockSize = 256; // You can adjust the block size according to your needs
+  // Configurar los bloques y los hilos
+  blockSize = 256; // Puedes ajustar el tamaño del bloque según tus necesidades
   numBlocks = (n + blockSize - 1) / blockSize;
   
+  // Asignar memoria para el resultado en el host y en el device
   result = (double*)malloc(sizeof(double));
   cudaMalloc((void**)&dev_result, sizeof(double));
   
+  // Inicializar el resultado en 0
   *result = 0.0;
   cudaMemcpy(dev_result, result, sizeof(double), cudaMemcpyHostToDevice);
   
+  // Medir el tiempo de inicio
   double start = getCurrentTime();
+  
+  // Lanzar el kernel en paralelo
   integrate<<<numBlocks, blockSize>>>(a, b, n, h, dev_result);
   cudaDeviceSynchronize();
+  
+  // Medir el tiempo de fin
   double end = getCurrentTime();
   
+  // Copiar el resultado de vuelta al host
   cudaMemcpy(result, dev_result, sizeof(double), cudaMemcpyDeviceToHost);
   
+  // Calcular el resultado de la integral
   integral = (h / 2) * (f(a) + f(b) + *result);
   
-  /*Print the answer */
-  printf("\nThe integral is: %lf\n", integral);
+  // Imprimir la respuesta
+  printf("\nEl resultado de la integral es: %lf\n", integral);
   
+  // Calcular el tiempo transcurrido
   double elapsedTime = end - start;
-  printf("Elapsed Time: %.6f seconds\n", elapsedTime);
+  printf("Tiempo transcurrido: %.6f segundos\n", elapsedTime);
   
-  // Calculation of speedup and scalability
+  // Cálculo del speedup y la escalabilidad
   int numThreads = numBlocks * blockSize;
   double sequentialTime = integral;
   double parallelTime = elapsedTime;
@@ -88,12 +104,13 @@ int main(){
   double scalability = sequentialTime / (parallelTime * numThreads);
   
   printf("Speedup: %.2f\n", speedup);
-  printf("Scalability: %.2f\n", scalability);
-
-  // New metric: Throughput
-  double throughput = integral / elapsedTime;
-  printf("Throughput: %.2f units per second\n", throughput);
+  printf("Escalabilidad: %.2f\n", scalability);
   
+  // Nueva métrica: Throughput
+  double throughput = integral / elapsedTime;
+  printf("Throughput: %.2f unidades por segundo\n", throughput);
+  
+  // Liberar memoria
   free(result);
   cudaFree(dev_result);
   
